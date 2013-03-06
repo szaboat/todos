ed4c69d359b9d564a095b23a20d884fc9943284e
Attila Szabo <attila.szabo@prezi.com>
2013-03-06 19:34:01 +0100

    Index view

--- todo/views.py
+++ todo/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.template.response import TemplateResponse 
+from todo.models import TodoItem
+
+def index(request):
+    todos = TodoItem.objects.all()
+    context = {'todos': todos}
+    return TemplateResponse(request, template='index.html', context=context)
