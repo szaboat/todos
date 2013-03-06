ff824e4129ce99d39e49c884c8f47952ea956df1
Attila Szabo <attila.szabo@prezi.com>
2013-03-06 23:15:03 +0100

    Edit todo

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns
-from todo.views import index
+from todo.views import index, edit
 
 urlpatterns = patterns('',
     (r'^$', index),
+    (r'^edit/(?P<id>\d+)/$', edit),
 )
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,31 @@
-from django.template.response import TemplateResponse 
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseBadRequest, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.template.response import TemplateResponse
+from todo.forms import TodoItemForm
 from todo.models import TodoItem
 
 def index(request):
     todos = TodoItem.objects.all()
     context = {'todos': todos}
     return TemplateResponse(request, template='index.html', context=context)
+
+def edit(request, id=None):
+    if id:
+        todo = get_object_or_404(TodoItem, pk=id)
+    else:
+        return HttpResponseBadRequest()
+
+    if request.POST:
+        form = TodoItemForm(request.POST, initial=todo)
+        if form.is_valid():
+            todo.name = form.cleaned_data.get('name')
+            todo.save()
+            redirect_url = reverse(index)
+            return HttpResponseRedirect(redirect_url)
+
+    else:
+        form = TodoItemForm(initial={'name': todo.name})
+
+    return TemplateResponse(request, 'edit.html', {'form': form})
+
